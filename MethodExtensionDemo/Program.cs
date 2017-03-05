b43e580ca2a4a1799c9e897489c77aa8506e4a11
sumit753 <sumit753@gmail.com>
2017-03-05 17:34:40 +0530

    First Commit

--- MethodExtensionDemo/Program.cs
+++ MethodExtensionDemo/Program.cs
@@ -26,7 +26,7 @@ namespace MethodExtensionDemo
 
             //Enumerable is wrapper class for Collection
 
-            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9,10 };
 
             //IEnumerable<int> output = numbers.Where(n => n % 2 == 0);
 
